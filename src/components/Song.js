9ab20ab05c102ef051489e59972504c6c5f0065f
lukemoenning <luke.moenning@gmail.com>
2022-11-04 23:48:03 -0500

    Display song info

--- src/components/Song.js
+++ src/components/Song.js
@@ -4,7 +4,17 @@ import './styles/Song.css';
 function Song({ track }) {
   return (
     <div className='song'>
-      <p>{track.name}</p>
+      {console.log(track)}
+      <img src={track?.album?.images[0]?.url} alt={track.name} height={40} width={40}></img>
+
+      <div className='nameAndArtist'>
+        <p>{track?.name}</p>
+        <p>{track?.artists?.map(artist => artist.name)}</p>
+      </div>
+  
+      <p>{track?.album?.name}</p>
+
+      <p>{track?.duration_ms}</p>
     </div>
   );
 }
