2090e35b5ca0e30a2fc6e0ae17923ee23aee5523
lukemoenning <luke.moenning@gmail.com>
2022-11-05 12:50:17 -0500

    Finish song display

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,3 +1,4 @@
+import { MusicNote } from '@mui/icons-material';
 import React from 'react';
 import './styles/Song.css';
 
@@ -5,16 +6,23 @@ function Song({ track }) {
   return (
     <div className='song'>
       {console.log(track)}
-      <img src={track?.album?.images[0]?.url} alt={track.name} height={40} width={40}></img>
 
-      <div className='nameAndArtist'>
-        <p>{track?.name}</p>
-        <p>{track?.artists?.map(artist => artist.name)}</p>
+      <div className='leftInfo'>
+
+        {/* If the album image exists display it, otherwise display the blank album image */}
+        {track?.album?.images[0] 
+          ? (<img src={track?.album?.images[0]?.url} alt={track.name} height={40} width={40}></img>)
+          : (<div className='blankAlbumImage'><MusicNote style={{ fontSize: 20}} /></div>)}
+
+        <div className='nameAndArtist'>
+          <p className='name'>{track?.name}</p>
+          <p className='artist'>{track?.artists?.map(artist => artist.name + ' ')}</p>
+        </div>
       </div>
-  
-      <p>{track?.album?.name}</p>
 
-      <p>{msToMinutes(track?.duration_ms)}</p>
+      <p className='album'>{track?.album?.name}</p>
+
+      <p className='duration'>{msToMinutes(track?.duration_ms)}</p>
     </div>
   );
 }
